35cb8b0a30ff9a79296ec7a690454790e2ad6c33
RomanZemlin <roman.zemlin@mail.ru>
2023-10-18 23:48:32 +0300

    h_w_23.2

--- users/models.py
+++ users/models.py
@@ -1,19 +1,50 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-NULLABLE = {'null': True, 'blank': True}
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        user = self.model(email=self.normalize_email(email))
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email=email, password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = None
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(verbose_name='Email', max_length=255, unique=True)
+    first_name = models.CharField(verbose_name='First Name', max_length=30, blank=True)
+    last_name = models.CharField(verbose_name='Last Name', max_length=30, blank=True)
+    is_active = models.BooleanField(verbose_name='Active', default=True)
+    is_staff = models.BooleanField(verbose_name='Staff', default=False)
+    is_admin = models.BooleanField(verbose_name='Admin', default=False)
 
-    avatar = models.ImageField(upload_to='avatars', **NULLABLE, verbose_name='Аватар')
-    telegram = models.CharField(max_length=255, **NULLABLE, verbose_name='Телеграм')
-    country = models.CharField(max_length=255, **NULLABLE, verbose_name='Страна')
+    avatar = models.ImageField(upload_to='avatars', null=True, blank=True, verbose_name='Аватар')
+    telegram = models.CharField(max_length=255, null=True, blank=True, verbose_name='Телеграм')
+    country = models.CharField(max_length=255, null=True, blank=True, verbose_name='Страна')
 
-    USERNAME_FIELD = "email"
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def __str__(self):
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def str(self):
         return self.email
